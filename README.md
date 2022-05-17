edfbd018c52dcfa2eb239e5c8022008e1af37981
acmbaker <104640776+acmbaker@users.noreply.github.com>
2022-05-17 17:46:40 +0100

    Changes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# fsjstechdegree-project-3
- 
+# Full Stack JavaScript Project 3
+
+For this project, we had to add JavaScript to create functionality and interactivity with the HTML form.
+
+## The Brief
+In this project, you'll use JavaScript to enhance an interactive registration form for a fictional Full Stack conference.
+
+Using the supplied HTML and CSS files, you'll add your own JavaScript to make the form more user-friendly by:
+
+Adding customized and conditional behavior and interactivity
+Validating user input and providing helpful error messages when the user enters invalid information into the form fields.
+
+## Exceeds Expectations
+Real-time error message
+On lines 157 - 169, I added an event listener for 'keyup' that will listen to any typing in the relevant fields and call the relevant functions from the event listeners.
+
+Conditional error message
+On lines 182 - 195, there are custom error messages that appear to enable the user to understand why their email input is or isn't working.
+
+## Outcome
+Everything is functioning as per the brief and exceeded grade requirements.
---
+++ js/script.js
@@ -0,0 +1,325 @@
+//Personal Details
+let nameField = document.querySelector("input#name");
+let emailField = document.querySelector("input#email");
+//Job
+let selectJobRole = document.querySelector("select#title");
+let otherField = document.querySelector("input#other-job-role");
+//Shirts
+let selectColorSection = document.querySelector("div#shirt-colors");
+let selectColorDropdown = document.querySelector("select#color");
+let selectDesign = document.querySelector("select#design");
+let jsPunsColors = document.querySelectorAll('option[data-theme="js puns"]');
+let heartJsColors = document.querySelectorAll('option[data-theme="heart js"]');
+//Activity list
+let activityList = document.querySelector("fieldset#activities");
+let total = document.querySelector("p#activities-cost");
+let totalAmount = 0;
+let activitiesBox = document.querySelector("div#activities-box");
+let checkBoxes = document.querySelectorAll(
+  'div#activities-box input[type="checkbox"]'
+);
+//Payments
+let creditCard = document.querySelector('option[value="credit-card"]');
+let paymentMethods = document.querySelector("fieldset.payment-methods");
+let creditCardSection = document.querySelector("div#credit-card");
+let paypalSection = document.querySelector("div#paypal");
+let bitcoinSection = document.querySelector("div#bitcoin");
+let form = document.querySelector("form");
+let selectedPaymentMethod = document.querySelector("#payment");
+let cardSelector = document.querySelector("input#cc-num");
+let zipSelector = document.querySelector("input#zip");
+let cvvSelector = document.querySelector("input#cvv");
+
+//Initial load events
+window.addEventListener("load", (e) => {
+  //Focus on the input name field
+  nameField.focus();
+  //Other field hide on initial load
+  otherField.style.display = "none";
+  //Select colour field hidden on initial load
+  selectColorSection.style.display = "none";
+  //Default credit card payment option selected
+  creditCard.setAttribute("selected", "");
+  //Hide PayPal and Bitcoin section on initial load
+  paypalSection.style.display = "none";
+  bitcoinSection.style.display = "none";
+});
+
+//Display other field if 'other' option is selected for job role
+selectJobRole.addEventListener("input", (e) => {
+  if (e.target.value === "other") {
+    otherField.style.display = "";
+  }
+});
+
+//Checking for which t-shirt design is selected and calling function
+selectDesign.addEventListener("change", (e) => {
+  if (e.target.value === "js puns") {
+    colorChoices("puns");
+  } else if (e.target.value === "heart js") {
+    colorChoices("heart");
+  }
+});
+
+//Function to display the right colours according to the t-shirt design selected
+function colorChoices(choice) {
+  selectColorSection.style.display = "";
+  if (choice === "puns") {
+    for (let i = 0; i < jsPunsColors.length; i++) {
+      jsPunsColors[i].style.display = "";
+    }
+    for (let i = 0; i < heartJsColors.length; i++) {
+      heartJsColors[i].style.display = "none";
+    }
+  } else if (choice === "heart") {
+    for (let i = 0; i < heartJsColors.length; i++) {
+      heartJsColors[i].style.display = "";
+    }
+    for (let i = 0; i < jsPunsColors.length; i++) {
+      jsPunsColors[i].style.display = "none";
+    }
+  }
+}
+
+//Register for activities section listener event + calculations
+activityList.addEventListener("change", (e) => {
+  //For loop to prevent activities with same time from being selected
+  for (let i = 0; i < checkBoxes.length; i++) {
+    if (
+      e.target.checked &&
+      e.target.getAttribute("data-day-and-time") ===
+        checkBoxes[i].getAttribute("data-day-and-time")
+    ) {
+      checkBoxes[i].disabled = true;
+      checkBoxes[i].parentNode.classList.add("disabled");
+      e.target.parentNode.classList.remove("disabled");
+      e.target.disabled = false;
+    }
+
+    if (
+      !e.target.checked &&
+      e.target.getAttribute("data-day-and-time") ===
+        checkBoxes[i].getAttribute("data-day-and-time")
+    ) {
+      checkBoxes[i].disabled = false;
+      checkBoxes[i].parentNode.classList.remove("disabled");
+      e.target.disabled = false;
+      e.target.parentNode.classList.remove("disabled");
+    }
+  }
+
+  let numberToAdd = parseInt(e.target.getAttribute("data-cost"));
+
+  if (e.target.checked) {
+    totalAmount += numberToAdd;
+  } else if (!e.target.checked) {
+    totalAmount -= numberToAdd;
+  }
+
+  total.textContent = `Total: $${totalAmount}`;
+});
+
+//Ensuring the focus is obvious for activities
+for (let i = 0; i < checkBoxes.length; i++) {
+  checkBoxes[i].addEventListener("focus", (e) => {
+    checkBoxes[i].parentNode.classList.add("focus");
+  });
+  checkBoxes[i].addEventListener("blur", (e) => {
+    checkBoxes[i].parentNode.classList.remove("focus");
+  });
+}
+
+//Checking for which payment method is selected and calling function
+paymentMethods.addEventListener("change", (e) => {
+  let value = e.target.value;
+  if (value === "credit-card" || value === "paypal" || value === "bitcoin") {
+    paymentSelector(value);
+  }
+});
+
+//Function for the payment selector, which sections display depending on selection
+function paymentSelector(choice) {
+  if (choice === "credit-card") {
+    creditCardSection.style.display = "";
+    paypalSection.style.display = "none";
+    bitcoinSection.style.display = "none";
+  } else if (choice === "paypal") {
+    creditCardSection.style.display = "none";
+    paypalSection.style.display = "";
+    bitcoinSection.style.display = "none";
+  } else if (choice === "bitcoin") {
+    creditCardSection.style.display = "none";
+    paypalSection.style.display = "none";
+    bitcoinSection.style.display = "";
+  }
+}
+
+//Active validations upon type
+nameField.addEventListener("keyup", (e) => {
+  nameValidation(e);
+});
+emailField.addEventListener("keyup", (e) => {
+  emailValidation(e);
+});
+activityList.addEventListener("change", (e) => {
+  activityValidation(e);
+});
+creditCardSection.addEventListener("keyup", (e) => {
+  creditCardValidation(e);
+});
+
+//Name validation
+function nameValidation(e) {
+  if (isValidName(nameField.value)) {
+    positiveValidation(nameField);
+  } else {
+    negativeValidation(nameField);
+    document.querySelector('span#name-hint').textContent = "Name must be at least 2 characters and contain no special characters."
+    e.preventDefault();
+  }
+}
+
+//Email validation
+function emailValidation(e) {
+  if (isValidEmail(emailField.value.toLowerCase())) {
+    positiveValidation(emailField);
+  } else if (emailField.value.length === 0){
+    negativeValidation(emailField);
+    document.querySelector('span#email-hint').textContent = "Please enter an email.";
+    e.preventDefault();
+  } else {
+    negativeValidation(emailField);
+    document.querySelector('span#email-hint').textContent = "Email doesn't include all three: @ . and tld";
+    e.preventDefault();
+  }
+}
+
+//Activity validation
+function activityValidation(e) {
+  //Activity checkboxes
+  if (isValidActivities(checkBoxes)) {
+    activitiesBox.style.borderColor = "";
+    activityList.classList.remove("not-valid");
+    activityList.classList.add("valid");
+    activityList.lastElementChild.style.display = "none";
+  } else {
+    activitiesBox.style.borderColor = "red";
+    activityList.classList.add("not-valid");
+    activityList.classList.remove("valid");
+    activityList.lastElementChild.style.display = "inline";
+    e.preventDefault();
+  }
+}
+
+//Credit card validation
+function creditCardValidation(e) {
+  //Credit card
+  if (selectedPaymentMethod.value === "credit-card") {
+    let cc = parseInt(cardSelector.value.replaceAll(" ", ""));
+    let zip = parseInt(zipSelector.value.replaceAll(" ", ""));
+    let cvv = parseInt(cvvSelector.value.replaceAll(" ", ""));
+
+    if (!isValidCC(cc, zip, cvv)) {
+      e.preventDefault();
+    }
+  }
+}
+
+//Form validation event listener for submission
+form.addEventListener("submit", (e) => {
+  nameValidation(e);
+  emailValidation(e);
+  activityValidation(e);
+  creditCardValidation(e);
+});
+
+//Checking the name entry is a valid
+function isValidName(name) {
+  let regex = /^[a-z ,.'-]+$/i;
+
+  //Checking that name matches regex & name length is greater than 1 character
+  if (regex.test(name) && name.length > 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Checking the email entry is valid
+function isValidEmail(email) {
+  //best regex for email validation taken from https://emailregex.com/
+  let regex =
+    /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  return regex.test(email);
+}
+
+//Checking that a checkbox has been selected
+function isValidActivities(checkBoxes) {
+  for (let i = 0; i < checkBoxes.length; i++) {
+    if (checkBoxes[i].checked) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//Checking that the card details are valid
+function isValidCC(card, zip, cvv) {
+  //Visa, MasterCard, American Express, Diners Club, Discover, and JCB cards
+  let regexForCD =
+    /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
+  //Allowing both the five-digit and nine-digit ZIP code formats
+  let regexForZip = /^[0-9]{5}(?:-[0-9]{4})?$/;
+  //Allows for 3 or 4 digit CVV's
+  let regexForCvv = /^[0-9]{3,4}$/;
+  let counter = 0;
+
+  //Card validation checks: test number 4484070000000000 / 4484 0700 0000 0000
+  if (regexForCD.test(card)) {
+    counter++;
+    positiveValidation(cardSelector);
+  } else {
+    counter--;
+    negativeValidation(cardSelector);
+  }
+
+  //Zip code validation checks: test number 12345
+  if (regexForZip.test(zip)) {
+    counter++;
+    positiveValidation(zipSelector);
+  } else {
+    counter--;
+    negativeValidation(zipSelector);
+  }
+
+  //CVV validation checks: test number 123
+  if (regexForCvv.test(cvv)) {
+    counter++;
+    positiveValidation(cvvSelector);
+  } else {
+    counter--;
+    negativeValidation(cvvSelector);
+  }
+
+  if (counter === 3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Function for negative validation to prevent duplication of code
+function negativeValidation(type) {
+  type.style.borderColor = "Red";
+  type.parentNode.classList.add("not-valid");
+  type.parentNode.classList.remove("valid");
+  type.parentNode.lastElementChild.style.display = "inline";
+}
+
+//Function for positive validation to prevent duplication of code
+function positiveValidation(type) {
+  type.style.borderColor = "";
+  type.parentNode.classList.remove("not-valid");
+  type.parentNode.classList.add("valid");
+  type.parentNode.lastElementChild.style.display = "none";
+}
